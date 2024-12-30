07ab99e6176223fac31ce0c15dcf6acc618deb1b
Avi <avibanerjee2000@gmail.com>
2024-12-31 01:20:44 +0530

    api has been called

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -12,6 +15,20 @@ type cliCommand struct {
 	name string
 	description string
 	callback func() error
+}
+
+type config struct {
+	Next string
+	Previous string
+}
+
+type LocationAreaResponse struct {
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
 }
 
 func main(){
@@ -31,20 +48,23 @@ func main(){
 		"map": {
 			name: "map",
 			description: "Displays the names of 20 location areas in the Pokemon world.",
-			callback: ,
-		}
+			callback: commandMap,
+		},
 	}
 	for scanner.Scan() {
 		userInput := scanner.Text()
 		userInput = strings.ToLower(userInput)
 		found := false
+		mapCalled := false
 		for _, cmd := range commands {
 			if cmd.name == userInput {
 				found = true
 				cmd.callback()
 			}
+			if userInput == "map" {
+				mapCalled = true
+			}
 		}
-		
 		if !found {
 			fmt.Println("Unknown command")
 		}
@@ -63,6 +83,29 @@ func commandHelp() error{
 	fmt.Println("help: Displays a help message")
 	fmt.Println("exit: Exit the Pokedex")
 	return errors.New("whatever")
+}
+
+func commandMap() error{
+	pokeUrl := "https://pokeapi.co/api/v2/location-area/?offset=20"
+	res, err := http.Get(pokeUrl)
+	if err != nil {
+		return fmt.Errorf("unable to fetch!: %s", err)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != err {
+		return fmt.Errorf("could not read response body: %s", err)
+	}
+	var data LocationAreaResponse
+	err = json.Unmarshal([]byte(body), &data)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal: %s", err)
+	}
+	locations := data.Results
+	for _, loc := range locations{
+		fmt.Println(loc.Name)
+	}
+	return fmt.Errorf("whatever")
 }
 
 func cleanInput(text string) []string{
