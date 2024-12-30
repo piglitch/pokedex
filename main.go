6ea22bc24041a2b14a0037a0b929d8ddf66a7fc7
Avi <avibanerjee2000@gmail.com>
2024-12-30 17:54:55 +0530

    repl program

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main(){
-	// fmt.Println("Hello, World!")	
-	fmt.Println(cleanInput("messi is not the goat"))
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex >")
+	for scanner.Scan() {
+		userInput := scanner.Text()
+		userInput = strings.ToLower(userInput)
+		words := strings.Fields(userInput)
+		fmt.Printf("Your command was: %s \n", words[0])
+		fmt.Print("Pokedex >")
+	}
 }
 
 func cleanInput(text string) []string{
